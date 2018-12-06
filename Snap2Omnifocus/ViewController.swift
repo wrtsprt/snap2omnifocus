fe0c3fa0cc5d89541c6cad54db010913eb7a28fd
Raphael Doehring <raphael.doehring@gmail.com>
2018-12-06 21:56:17 +0100

    Basic implementation of take image -> call Omnifocus

--- Snap2Omnifocus/ViewController.swift
+++ Snap2Omnifocus/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        imagePicker.allowsEditing = false
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.present(imagePicker, animated: true, completion: nil)
     }
 
 
 }
-
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        debugPrint("image got poocken")
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        let imageData:Data = image.jpegData(compressionQuality: 0.7)!
+        let imageString = imageData.base64EncodedString()
+        
+        let ofUrl = "omnifocus:///add?name=MyImage&attachment-name=img.png&attachment=" + imageString
+        
+        UIApplication.shared.open(URL(string: ofUrl)!)
+    }
+    
+}
+extension ViewController : UINavigationControllerDelegate {
+    
+}
