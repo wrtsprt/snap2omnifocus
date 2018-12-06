b60bdfe99ea7acd14ad50bd7918abe2d7938f880
Raphael Doehring <raphael.doehring@gmail.com>
2018-12-06 20:42:54 +0100

    Initial Commit

---
+++ Snap2Omnifocus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snap2Omnifocus
+//
+//  Created by rap on 06/12/2018.
+//  Copyright © 2018 me.wrtsprt.labs.rrr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
